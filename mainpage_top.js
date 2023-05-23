fb0795a7f439e3d86f0636d4f89ffdb437615ed9
hxxnjoon <105426026+hxxnjoon@users.noreply.github.com>
2023-05-24 01:42:30 +0900

    버튼이랑 모달창 완료

---
+++ mainpage_top.js
@@ -0,0 +1,33 @@
+const modal = document.getElementById("modal")
+    function modalOn() {
+        modal.style.display = "flex"
+        document.body.style.overflow = "hidden";
+    }
+    function modalOff() {
+        modal.style.display = "none"
+        document.body.style.overflow = "";
+    }
+    const btnModal = document.getElementById("btn-modal")
+    btnModal.addEventListener("click", e => {
+        modalOn()
+    })
+    const closeBtn = modal.querySelector(".close-area")
+    closeBtn.addEventListener("click", e => {
+        modalOff()
+    })
+
+    modal.addEventListener("click", e => {
+        const evTarget = e.target
+        if(evTarget.classList.contains("modal-overlay")) {
+            modal.style.display = "none"
+        }
+    })
+
+    modalOff();
+
+    var buttons = document.getElementsByClassName("menubtn");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+    });
+  }
