728c9117af2ad40c6531641968e8da084ace6518
gimreuna <kra0220m@gmail.com>
2023-05-24 21:17:56 +0900

    검색조건에 따라 나오는 결과페이지 - 수정 !

--- search_condition.js
+++ search_condition.js
@@ -38,41 +38,41 @@
         button.onclick = function (event) {
 
 
-          if((a!="group") && (b=="loudness") && ((c=="10000") || (c=="20000")) && (d=="korea") )
+          if((a==4) && (b=="loudness") && (c==20000) && (d=="korea") )
                   {location.href = 'result.html';}   //팔각도 , 단골손님, 대동집, 땅코 , 쭈꾸미도사, 오징어바다, 곱창하우스
-          if((a!="group") && (b=="quiet") && ((c=="10000") || (c=="20000")) && (d=="japan") )
-                  {location.href = 'result.html';} // 오감 
-          if((a!="group") && (b=="loudness") && (c=="10000")  && (d=="korea") )
-                  {location.href = 'result.html';}   //이돈집 
-          if((a!="group") && (b=="quiet") && (c=="30000")&& (d=="japan") )
-                  {location.href = 'result.html';}   //여운, 와시리 
-          if((a!="group") && (b=="loudness") && ((c=="10000") || (c=="20000")) && (d=="korea") )
-                  {location.href = 'result.html';}  
+          if((a==4) && (b=="quiet") && (c==20000) && (d=="japan") )
+                  {location.href = 'categoryresult.html';} // 오감 
+          if((a==4) && (b=="loudness") && (c==20000)  && (d=="korea") )
+                  {location.href = 'categoryresult.html';}   //이돈집 
+          if((a==4) && (b=="quiet") && (c==30000)&& (d=="japan") )
+                  {location.href = 'categoryresult.html';}   //여운, 와시리 
+          if((a==4) && (b=="loudness") && (c==20000) && (d=="korea") )
+                  {location.href = 'categoryresult.html';}  
          
-          if(((a=="group") || (a=="4")) && (b=="loudness") && ((c=="10000") || (c=="20000")) && (d=="etc") )
-                  {location.href = 'result.html';}  //갱생
-          if((a!="group") && (b=="loudness") && ((c=="10000") || (c=="20000")) && (d=="etc") )
-                  {location.href = 'result.html';}//리퀴진
-          if((a!="group") && (b=="quiet") && ((c=="30000") || (c=="over")) && (d=="korea") )
-                  {location.href = 'result.html';}//육회공작소
-          if(((a=="group") || (a=="4")) && (b=="loudness") && ((c=="10000") || (c=="20000")) && (d=="korea") )
-                  {location.href = 'result.html';}  //구이마을, 이가갈비, 장모족발 
-          if((a!="group") && (b=="loudness") && ((c=="10000") || (c=="20000")) && (d=="china") )
-                  {location.href = 'result.html';}   //용용선생 
-          if((a!="group") && (b=="loudness") && (c=="30000") && (d=="korea") )
-                  {location.href = 'result.html';}   //제일곱창 
-          if((a!="group") && (b=="quiet") && ((c=="10000") || (c=="20000")) && (d=="korea") )
-                  {location.href = 'result.html';} // 종로계림
-          if((a!="group") && (b=="quiet") && ((c=="10000") || (c=="20000")) && (d=="etc") )
-                  {location.href = 'result.html';} // 포차윤 
-          if(((a=="group") || (a=="4")) && (b=="quiet") && (c=="30000") && (d=="korea") )
-                  {location.href = 'result.html';}  //한양대육회집
-          if(((a=="group") || (a=="4")) && (b=="quiet") && ((c=="10000") || (c=="20000")) && (d=="korea") )
-                  {location.href = 'result.html';}  //한양포차 
-          if((a!="group") && (b=="loudness") && (c!="over")  && (d=="korea") )
-                  {location.href = 'result.html';}  //메종드
-          if((a!="group") && (b=="loudness") && (c=="over")  && (d=="korea") )
-                  {location.href = 'result.html';}  //부엉이산장 
+          if(((a==5) || (a=="4")) && (b=="loudness") && (c==20000) && (d=="etc") )
+                  {location.href = 'categoryresult.html';}  //갱생
+          if((a==4) && (b=="loudness") && (c==20000) && (d=="etc") )
+                  {location.href = 'categoryresult.html';}//리퀴진
+          if((a==4) && (b=="quiet") && (c==30000) && (d=="korea") )
+                  {location.href = 'categoryresult.html';}//육회공작소
+          if(((a==5) || (a=="4")) && (b=="loudness") && (c==20000) && (d=="korea") )
+                  {location.href = 'categoryresult.html';}  //구이마을, 이가갈비, 장모족발 
+          if((a==4) && (b=="loudness") && (c==20000) && (d=="china") )
+                  {location.href = 'categoryresult.html';}   //용용선생 
+          if((a==4) && (b=="loudness") && (c==30000) && (d=="korea") )
+                  {location.href = 'categoryresult.html';}   //제일곱창 
+          if((a==4) && (b=="quiet") && (c==20000) && (d=="korea") )
+                  {location.href = 'categoryresult.html';} // 종로계림
+          if((a==4) && (b=="quiet") && (c==20000) && (d=="etc") )
+                  {location.href = 'categoryresult.html';} // 포차윤 
+          if(((a==5) || (a=="4")) && (b=="quiet") && (c==30000) && (d=="korea") )
+                  {location.href = 'categoryresult.html';}  //한양대육회집
+          if(((a==5) || (a=="4")) && (b=="quiet") && (c==20000) && (d=="korea") )
+                  {location.href = 'categoryresult.html';}  //한양포차 
+          if((a==4) && (b=="loudness") && (c==30000)  && (d=="korea") )
+                  {location.href = 'categoryresult.html';}  //메종드
+          if((a==4) && (b=="loudness") && (c==30000)  && (d=="korea") )
+                  {location.href = 'categoryresult.html';}  //부엉이산장 
 
                   
           
