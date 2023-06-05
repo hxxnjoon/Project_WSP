5f363a2c1d8e39c5c336a5d5ecb374e09543e0a8
hxxnjoon <105426026+hxxnjoon@users.noreply.github.com>
2023-06-06 04:19:59 +0900

    카테고리 검색 완성 *4인이하 -> 3인이하, 4인이상->5인이상으로 수정 *개수를 조금 맞추는 게 좋을 듯

--- category.js
+++ category.js
@@ -1,92 +1,120 @@
+function resetButtons() {
+  var buttons = document.getElementsByClassName("menubtn");
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("active");
+  }
+}
 
+function resetSearch() {
+
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(checkbox => {
+    checkbox.checked = false;
+  });
+}
      let lists=[
-     {name:"팔각도", person: "4", mood: "loudness", price:"20000",food:"korea"}, 
-     {name:"단골손님", person: "4", mood: "loudness", price:"20000",food:"korea"},
-     {name:"대동집", person: "4", mood: "loudness", price:"20000",food:"korea"},
-     {name:"땅코참숯구이", person: "4", mood: "loudness", price:"20000",food:"korea"},
-     {name:"쭈꾸미도사", person: "4", mood: "loudness", price:"20000",food:"korea"},
+     {name:"팔각도", person: "3", mood: "loudness", price:"20000",food:"korea"}, 
+     {name:"단골손님", person: "3", mood: "loudness", price:"20000",food:"korea"},
+     {name:"대동집", person: "3", mood: "loudness", price:"20000",food:"korea"},
+     {name:"땅코참숯구이", person: "3", mood: "loudness", price:"20000",food:"korea"},
+     {name:"쭈꾸미도사", person: "3", mood: "loudness", price:"20000",food:"korea"},
      
-     {name:"싱싱오징어바다", person: "4", mood: "loudness", price:"20000",food:"korea"},
-     {name:"곱창하우스", person: "4", mood: "loudness", price:"20000",food:"korea"},
-     {name:"오감", person: "4", mood: "quiet", price:"20000",food:"japan"},
-     {name:"이돈집", person: "4", mood: "loudness", price:"20000",food:"korea"},
-     {name:"여운", person: "4", mood: "quiet", price:"30000",food:"japan"},
+     {name:"싱싱오징어바다", person: "3", mood: "loudness", price:"20000",food:"korea"},
+     {name:"곱창하우스", person: "3", mood: "loudness", price:"20000",food:"korea"},
+     {name:"오감", person: "3", mood: "quiet", price:"20000",food:"japan"},
+     {name:"이돈집", person: "3", mood: "loudness", price:"20000",food:"korea"},
+     {name:"여운", person: "3", mood: "quiet", price:"30000",food:"japan"},
      
-     {name:"와시리", person: "4", mood: "quiet", price:"30000",food:"japan"},
-     {name:"갱생", person: "5", mood: "loudness", price:"20000",food:"etc"},
-     {name:"리퀴진", person: "4", mood: "loudness", price:"20000",food:"etc"},
-     {name:"육회공작소", person: "4", mood: "quiet", price:"30000",food:"korea"},
-     {name:"돼지꿈", person: "5", mood: "loudness", price:"20000",food:"korea"},
+     {name:"와시리", person: "3", mood: "quiet", price:"30000",food:"japan"},
+     {name:"갱생", person: "4", mood: "loudness", price:"20000",food:"etc"},
+     {name:"리퀴진", person: "3", mood: "loudness", price:"20000",food:"etc"},
+     {name:"육회공작소", person: "3", mood: "quiet", price:"30000",food:"korea"},
+     {name:"돼지꿈", person: "4", mood: "loudness", price:"20000",food:"korea"},
     
-     {name:"이가갈비", person: "5", mood: "loudness", price:"20000",food:"korea"},
-     {name:"장모족발", person: "5", mood: "loudness", price:"20000",food:"korea"},
-     {name:"용용선생", person: "4", mood: "loudness", price:"20000",food:"china"},
-     {name:"제일곱창", person: "4", mood: "loudness", price:"30000",food:"korea"},
-     {name:"종로계림닭도리탕원조", person: "4", mood: "quiet", price:"20000",food:"korea"},
+     {name:"이가갈비", person: "4", mood: "loudness", price:"20000",food:"korea"},
+     {name:"장모족발", person: "4", mood: "loudness", price:"20000",food:"korea"},
+     {name:"용용선생", person: "3", mood: "loudness", price:"20000",food:"china"},
+     {name:"제일곱창", person: "3", mood: "loudness", price:"30000",food:"korea"},
+     {name:"종로계림닭도리탕원조", person: "3", mood: "quiet", price:"20000",food:"korea"},
      
-     {name:"포차윤", person: "4", mood: "quiet", price:"20000",food:"etc"},
-     {name:"한양대육회집", person: "5", mood: "quiet", price:"30000",food:"korea"},
-     {name:"한양포차", person: "5", mood: "quiet", price:"20000",food:"korea"},
-     {name:"메종드혁이네", person: "4", mood: "loudness", price:"30000",food:"korea"},
-     {name:"부엉이산장", person: "4", mood: "loudness", price:"30000",food:"korea"}
+     {name:"포차윤", person: "3", mood: "quiet", price:"20000",food:"etc"},
+     {name:"한양대육회집", person: "4", mood: "quiet", price:"30000",food:"korea"},
+     {name:"한양포차", person: "4", mood: "quiet", price:"20000",food:"korea"},
+     {name:"메종드혁이네", person: "3", mood: "loudness", price:"30000",food:"korea"},
+     {name:"부엉이산장", person: "3", mood: "loudness", price:"30000",food:"korea"}
      ];
 
-     function catesrc() {
-
-      document.getElementById('hotnew').style.display = "none";
-      document.getElementById('alcholdict').style.display = "none";
+    let personchecked;
+    let moodchecked;
+    let pricechecked;
+    let foodchecked;
     
-      document.getElementById('searchsection').style.display = "block";
-      
-      let personchecked=[];
-      let moodchecked=[];
-      let pricechecked=[];
-      let foodchecked=[];
+function catesrc() {
 
-      personchecked = Array.from(document.querySelectorAll('input[name="person"]:checked')).map(input => input.value);
-      moodchecked = Array.from(document.querySelectorAll('input[name="mood"]:checked')).map(input => input.value);
-      pricechecked = Array.from(document.querySelectorAll('input[name="price"]:checked')).map(input => input.value);
-      foodchecked = Array.from(document.querySelectorAll('input[name="food"]:checked')).map(input => input.value);
-    
-      let resultList = [];
+  document.getElementById('hotnew').style.display = "none";
+  document.getElementById('alcholdict').style.display = "none";
+  document.getElementById('searchsection').style.display = "block";
 
-      for (let j = 0; j < lists.length; j++) {
-        if (
-          (personchecked.includes(lists[j].person) || personchecked.length === 0) &&
-          (moodchecked.includes(lists[j].mood) || moodchecked.length === 0) &&
-          (pricechecked.includes(lists[j].price) || pricechecked.length === 0) &&
-          (foodchecked.includes(lists[j].food) || foodchecked.length === 0)
-        ) {
-          resultList.push(lists[j].name);
-          
-        }
+  
+  personchecked = Array.from(document.querySelectorAll('input[name="person"]:checked')).map(input => input.value);
+  moodchecked = Array.from(document.querySelectorAll('input[name="mood"]:checked')).map(input => input.value);
+  pricechecked = Array.from(document.querySelectorAll('input[name="price"]:checked')).map(input => input.value);
+  foodchecked = Array.from(document.querySelectorAll('input[name="food"]:checked')).map(input => input.value);
+
+  console.log(personchecked);
+  console.log(moodchecked);
+  console.log(pricechecked);
+  console.log(foodchecked);
+
+  performSearch();
+}
+
+function performSearch() {
+  
+  let resultList = [];
+
+  for (let j = 0; j < lists.length; j++) {
+    if (
+      (personchecked.includes(lists[j].person) || personchecked.length === 0) &&
+      (moodchecked.includes(lists[j].mood) || moodchecked.length === 0) &&
+      (pricechecked.includes(lists[j].price) || pricechecked.length === 0) &&
+      (foodchecked.includes(lists[j].food) || foodchecked.length === 0)
+    ) {
+      resultList.push(lists[j].name);
+    }
+  }
+
+  
+  console.log(resultList);
+
+  var items = document.getElementsByClassName("항목");
+
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    var shouldBeDisplayed = false;
+
+    for (let j = 0; j < resultList.length; j++) {
+      if (resultList[j] === item.id) {
+        shouldBeDisplayed = true;
+        break;
       }
+    }
 
-        var items = document.getElementsByClassName("항목");
+    if (shouldBeDisplayed) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  }
 
-        for (var i = 0; i < items.length; i++) {
-          var item = items[i];
-          var shouldBeDisplayed = false;
-      
-          for (let j = 0; j < resultList.length; j++) {
-            if (resultList[j] === item.id) {
-              shouldBeDisplayed = true;
-              break;
-            }
-          }
-      
-          if (shouldBeDisplayed) {
-            item.style.display = "block";
-          } else {
-            item.style.display = "none";
-          }
-        }
-      
-        if (resultList.length > 0) {
-          document.getElementById('searchresult').innerHTML = "카테고리 검색 결과";
-        } else {
-          document.getElementById('searchresult').innerHTML = "검색 결과 없음";
-        }
-      
-        modalOff();
-      } 
+  if (resultList.length > 0) {
+    document.getElementById('searchresult').innerHTML = "카테고리 검색 결과";
+  } else {
+    document.getElementById('searchresult').innerHTML = "검색 결과 없음";
+  }
+
+  modalOff();
+  resetSearch();
+  resetButtons();
+
+}
